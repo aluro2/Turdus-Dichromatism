e94239ac104c3be52a2507ffc35a27dbd9335ce9
Alec Luro <alec.b.luro@gmail.com>
2021-03-12 16:02:32 -0600

    name container for consistency

--- docker_rstudio.sh
+++ docker_rstudio.sh
@@ -3,4 +3,4 @@
 echo "Starting temporary docker container. No changes to container will be saved!"
 
 # Run Temporary Docker Container
-docker run --rm -d -p 8787:8787 -v $(pwd):/home/rstudio -e DISABLE_AUTH=true turdus_analyses_container:latest
+docker run --rm -d -p 8787:8787 -v $(pwd):/home/rstudio -e DISABLE_AUTH=true --name turdus-analysis-rstudio turdus_analyses_container:latest
