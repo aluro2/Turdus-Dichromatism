3d12da821265ae7106b959945e24cde7e4c5524f
Alec Luro <alec.b.luro@gmail.com>
2021-04-09 15:17:07 -0500

    ms conversion

--- Bash-Scripts/convert-manuscript.sh
+++ Bash-Scripts/convert-manuscript.sh
@@ -4,8 +4,12 @@ cd Manuscript
 
 libreoffice --headless --convert-to odt turdus-dichromatism-ABL.fodt
 
+echo "Conversion to .odt complete!"
+
 libreoffice --headless --convert-to docx turdus-dichromatism-ABL.odt
 
 echo "Conversion to .docx complete!"
 
-rm turdus-dichromatism-ABL.odt
+pandoc -s turdus-dichromatism-ABL.odt -t markdown -o turdus-dichromatism-ABL.md
+
+echo "Conversion to .md complete!"
