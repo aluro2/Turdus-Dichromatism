46ad62dd43a5c7ba11eaabe54b672bfd600a443e
Alec Luro <alec.b.luro@gmail.com>
2021-04-16 17:07:32 -0500

    unstage bash scripts

--- Bash-Scripts/docker_get_docx_changes.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-
-
-# Convert docx to Markdown with tracked changes
-docker run --rm -v $PWD:/pandoc -w /pandoc/ dalibo/pandocker --citeproc --track-changes=all -f docx Manuscript/turdus-dichromatism-ABL.docx -o Manuscript/DOCX-CHANGES.md
--- Bash-Scripts/update-google-drive-MS.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-
-
-
-# Copy docx ver of MS to Google Drive
-echo "Updating Google Drive Illinois copy of MS..."
-cp Manuscript/turdus-dichromatism-ABL.docx ~/Google-Drive-Illinois/Manuscripts/Turdus-Dichromatism-2021/
-echo "DONE"
